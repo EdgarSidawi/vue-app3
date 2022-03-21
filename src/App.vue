d852d3ba931d728b76237127687ac8c4b201709b
Edgar <romantibuai@gmail.com>
2022-03-21 16:31:11 +0000

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,59 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container">
+    <h6 class="text-center text-danger">
+      Welcome to the Vuejs App 3 testing area
+    </h6>
+    <form @submit.prevent>
+      <div class="form-group">
+        <label for="formGroupExampleInput">My Todos</label>
+        <input
+          type="text"
+          v-model="todo"
+          class="form-control"
+          id="formGroupExampleInput"
+          placeholder="my todos"
+        />
+      </div>
+    </form>
+    <button class="btn btn-primary" @click="addTodo">Add</button>
+    <div>
+      <br /><br />
+
+      <TodoList :lists="lists" @deleteItem="deleteTodo" />
+    </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import { ref } from "vue";
+import TodoList from "./components/TodoList.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
-</script>
+    TodoList,
+  },
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+  setup() {
+    let todo = ref("");
+    let lists = ref([]);
+
+    function addTodo() {
+      if (todo.value) {
+        lists.value.push(todo.value);
+        todo.value = "";
+      }
+    }
+    function deleteTodo(index) {
+      lists.value.splice(index, 1);
+    }
+
+    return {
+      todo,
+      lists,
+      addTodo,
+      deleteTodo,
+    };
+  },
+};
+</script>
---
+++ src/components/TodoList.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <div  v-for="(list,index) in lists" :key="index">
+      <li @click="deleteTodo(index)">
+            {{list}}
+      </li>
+    </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'TodoList',
+  props: ['lists'],
+
+  setup(_, context) {
+
+    function deleteTodo(index) { 
+     context.emit("deleteItem",index)
+    }
+    return{
+      deleteTodo
+    }
+  }
+}
+</script>
